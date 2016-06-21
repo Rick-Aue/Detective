6ba3f8ed1a13c7ebe4232552286c5253cbfa8ff0
Rick <Rauedragon@hotmail.com>
2016-06-21 17:33:14 +0200

    Detective

--- RPG Detective June experimenteel/Clue.java
+++
@@ -1,7 +0,0 @@
-package rpgDetective;
-
-//WIP Items die de detective vindt en in zijn zak stopt, moet nog uitgewerkt worden.
-public class Clue {
-	String[] items = {"item1", "item2", "item3", "item4", "item5"};
-	static int aanwijzingen = 0;
-}
